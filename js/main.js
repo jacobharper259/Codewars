991ca9ce94f220cd95bdeb30d31de55a71c83f3a
jacobharper259 <jacobharper259@gmail.com>
2022-05-14 08:02:27 -0400

    slacking

--- js/main.js
+++ js/main.js
@@ -23,17 +23,43 @@ function getSum(a,b){
   }
 */
 //Good Code
+/*
 const GetSum = (a, b) => {
     let min = Math.min(a, b),
         max = Math.max(a, b);
     return (max - min + 1) * (min + max) / 2;
   }
-
+*/
 /*
   Clock shows h hours, m minutes and s seconds after midnight.
 
   Your task is to write a function which returns the time since midnight in milliseconds.
 */
+/*
 function past(h, m, s){
     return (s*1000) + (m*60000) + (h* 3600000)
   }
+
+
+function accum(s) {
+    let ans = []
+    ans.push(s.charAt(0))
+	for(let i = 0; i<s.length;i++){
+        
+        for(l=0;l<i;l++){
+            ans.push(s.charAt(i))
+        }
+  }
+  console.log(ans)
+  
+}
+*/
+function lovefunc(flower1, flower2){
+  if(flower1%2===0 && flower2%2!==0){
+    return true
+  }else if(flower2%2===0 && flower1%2!==0){
+    return true
+  }else{
+    return false
+  }
+}
